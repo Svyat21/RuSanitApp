651733c76c0b5952107e56e0bc5072f572f5491b
sursv <sursvyat@gmail.com>
2021-08-16 19:58:54 +0300

    initial construction of the model

--- rusanitapp/shop/models.py
+++ rusanitapp/shop/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Наименование')
+    price = models.IntegerField(verbose_name='Цена')
+    people_amount = models.IntegerField(verbose_name='Рекомендуемое количество пользователей')
+    short_description = models.CharField(max_length=255, verbose_name='Краткое описание')
+    full_description = models.TextField(verbose_name='Полное описание продукта')
+    # services =
+    in_stock = models.IntegerField(verbose_name='остаток на стоке')
+    # customer =
+    is_published = models.BooleanField(default=False, verbose_name='Опубликован')
+    main_photo = models.ImageField(upload_to='photos/', verbose_name='Главное фото')
+    # photo_album =
+    date_update = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
+    date_create = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    # slug =
+
+    def __str__(self):
+        return f'{self.pk}. {self.name}'
--- rusanitapp/shop/views.py
+++ rusanitapp/shop/views.py
@@ -1,5 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from shop.models import *
+
 
 def index(request):
-    return HttpResponse('Страница приложения Shop')
+    products = Product.objects.all()
+    context = {
+        'title_html': 'Главная страница',
+        'title': 'Заголовок главной страницы',
+        'products': products
+    }
+    return render(request, 'shop/index.html', context=context)
+
+
+def product(request, prod_id):
+    return HttpResponse(f'Конкретный продукт с номером id - {prod_id}.')
