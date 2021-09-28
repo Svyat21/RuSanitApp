5ce0d4b5e3812a1e428c2c46294d74a22a3928b9
sursv <sursvyat@gmail.com>
2021-09-28 20:05:09 +0300

    fixed a bug in the condition check

--- rusanitapp/shop/static/shop/js/script.js
+++ rusanitapp/shop/static/shop/js/script.js
@@ -34,7 +34,8 @@ let payWarden = new MutationObserver(function () {
 let deliveryWarden = new MutationObserver(function () {
     getDeliveryResult();
 });
-if (trackedPay) {
+if (trackedPay.item(0)) {
+    console.log(trackedPay.item(0));
     payWarden.observe(trackedPay.item(0), {childList: true, subtree: true, attributes: true, characterData: true});
     deliveryWarden.observe(trackedDelivery.item(0), {childList: true, subtree: true, attributes: true, characterData: true});
 }
